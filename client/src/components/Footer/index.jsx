26dae275b388e122975a4ecaef94b96922b7c8c1
levion8304 <ngocliem10a5nth@gmail.com>
2024-10-19 10:39:14 +0700

    Hoàn thành khung Footer, chưa style

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -76,21 +76,308 @@ function Footer() {
               </li>
             </ul>
           </Col>
-          <Col span={4.8}></Col>
-          <Col span={4.8}></Col>
+          <Col span={4.8}>
+            <h5>Phương thức thanh toán</h5>
+            <img
+              src={
+                "../../../public/assets/images/Screenshot 2024-10-19 100252.png"
+              }
+              alt=""
+            />
+            <h5>Dịch vụ giao hàng</h5>
+            <img
+              src={
+                "../../../public/assets/images/e71563afb23e3f34a148fe1b7d3413c5.png"
+              }
+              alt=""
+            />
+          </Col>
+          <Col span={4.8}>
+            <h5>Kết nối với chúng tôi</h5>
+            <h5>Tải ứng dụng trên điện thoại</h5>
+            <img src={"../../../public/assets/images/qrcode.png"} alt="" />
+            <img
+              src={"../../../public/assets/images/appstore.png.png"}
+              alt=""
+            />
+            <img src={"../../../public/assets/images/playstore.png"} alt="" />
+          </Col>
         </Row>
         <Row>
-          <Col span={24}></Col>
+          <Col span={24}>
+            <h2>Công ty TNHH TI KI</h2>
+            <p>
+              Tòa nhà số 52 đường Út Tịch, Phường 4, Quận Tân Bình, Thành phố Hồ
+              Chí Minh
+              <br />
+              Giấy chứng nhận đăng ký doanh nghiệp số 0309532909 do Sở Kế Hoạch
+              và Đầu Tư Thành phố Hồ Chí Minh cấp lần đầu vào ngày 06/01/2010.
+              <br />
+              Hotline: 1900 6035
+            </p>
+          </Col>
         </Row>
         <Row>
-          <Col span={24}></Col>
+          <Col span={24}>
+            <h2>Tiki - Thật nhanh, thật chất lượng, thật rẻ</h2>
+            <p>
+              <h5>Tiki có tất cả</h5>
+              Với hàng triệu sản phẩm từ các thương hiệu, cửa hàng uy tín, hàng
+              nghìn loại mặt hàng từ Điện thoại smartphone tới Rau củ quả tươi,
+              kèm theo dịch vụ giao hàng siêu tốc TikiNOW, Tiki mang đến cho bạn
+              một trải nghiệm mua sắm online bắt đầu bằng chữ tín. Thêm vào đó,
+              ở Tiki bạn có thể dễ dàng sử dụng vô vàn các tiện ích khác như mua
+              thẻ cào, thanh toán hoá đơn điện nước, các dịch vụ bảo hiểm.
+            </p>
+            <p>
+              <h5>Khuyến mãi, ưu đãi tràn ngập</h5>
+              Bạn muốn săn giá sốc, Tiki có giá sốc mỗi ngày cho bạn! Bạn là tín
+              đồ của các thương hiệu, các cửa hàng Official chính hãng đang chờ
+              đón bạn. Không cần săn mã freeship, vì Tiki đã có hàng triệu sản
+              phẩm trong chương trình Freeship+, không giới hạn lượt đặt, tiết
+              kiệm thời gian vàng bạc của bạn. Mua thêm gói TikiNOW tiết kiệm để
+              nhận 100% free ship 2h & trong ngày, hoặc mua gói TikiNOW cao cấp
+              để nhận được 100% freeship, áp dụng cho 100% sản phẩm, 100% tỉnh
+              thành Việt Nam. Bạn muốn tiết kiệm hơn nữa? Đã có TikiCARD, thẻ
+              tín dụng Tiki hoàn tiền 15% trên mọi giao dịch (tối đa hoàn
+              600k/tháng)
+            </p>
+          </Col>
         </Row>
         <Row>
-          <Col span={4.8}></Col>
-          <Col span={4.8}></Col>
-          <Col span={4.8}></Col>
-          <Col span={4.8}></Col>
-          <Col span={4.8}></Col>
+          <Col span={24}>
+            <h2>Thương Hiệu Nổi Bật</h2>
+            <p>
+              vascara / dior / esteelauder / th truemilk / barbie / owen /
+              ensure / durex / bioderma / elly / milo / skechers / aldo /
+              triumph / nutifood / kindle / nerman / wacom / anessa / yoosee /
+              olay / similac / comfort / bitas / shiseido / langfarm / hukan /
+              vichy / fila / tsubaki
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <h2>Danh mục sản phẩm</h2>
+          <Col span={4.8}>
+            <div>
+              <h5>Đồ Chơi - Mẹ & Bé</h5>
+              <p>
+                Thời Trang Cho Mẹ Và Bé / Đồ chơi / Đồ dùng cho bé / Chăm sóc
+                nhà cửa / Chăm sóc mẹ mang thai, sau sinh / Dinh dưỡng cho bé /
+                Tã, Bỉm / Dinh dưỡng cho người lớn / Dinh dưỡng cho mẹ / Thực
+                phẩm ăn dặm / Chuẩn bị mang thai
+              </p>
+            </div>
+            <div>
+              <h5>Thực Phẩm Tươi Sống</h5>
+              <p>
+                Trái Cây / Thịt, Trứng / Cá, thuỷ hải sản / Rau củ quả / Thực
+                phẩm Việt / Sữa, bơ, phô mai / Đông lạnh, mát / Dầu ăn, gia vị /
+                Gạo, mì, nông sản / Đồ hộp, đóng gói / Bia, đồ uống / Thực phẩm
+                chay / Dành cho trẻ em / Bánh kẹo, giỏ quà / Thức ăn, đồ thú
+                cưng / Chăm sóc cá nhân / Chăm sóc nhà cửa
+              </p>
+            </div>
+            <div>
+              <h5>Điện Thoại - Máy Tính Bảng</h5>
+              <p>
+                Điện thoại Smartphone / Điện thoại bàn / Điện thoại phổ thông /
+                Máy đọc sách / Máy tính bảng
+              </p>
+            </div>
+            <div>
+              <h5>Làm Đẹp - Sức Khỏe</h5>
+              <p>
+                Chăm sóc da mặt / Dụng cụ làm đẹp / Thực phẩm chức năng / Trang
+                điểm / Chăm sóc cơ thể / Máy Massage & Thiết bị chăm sóc sức
+                khỏe / Sản phẩm thiên nhiên & Khác / Chăm sóc tóc và da đầu /
+                Chăm sóc cá nhân / Nước hoa / Hỗ trợ tình dục / Bộ sản phẩm làm
+                đẹp / Dược mỹ phẩm
+              </p>
+            </div>
+            <div>
+              <h5>Điện Gia Dụng</h5>
+              <p>Đồ dùng nhà bếp / Thiết bị gia đình</p>
+            </div>
+          </Col>
+          <Col span={4.8}>
+            <div>
+              <h5>Thời trang nữ</h5>
+              <p>
+                Áo nữ / Đầm nữ / Quần nữ / Áo liền quần - Bộ trang phục / Áo
+                khoác nữ / Đồ ngủ - Đồ mặc nhà nữ / Chân váy / Trang phục bơi nữ
+                / Thời trang bầu và sau sinh / Thời trang trung niên
+              </p>
+            </div>
+            <div>
+              <h5>Thời trang nam</h5>
+              <p>
+                Áo thun nam / Quần nam / Áo vest - Áo khoác nam / Áo sơ mi nam /
+                Đồ lót nam / Áo hoodie nam / Đồ ngủ, đồ mặc nhà nam / Đồ đôi -
+                Đồ gia đình nam / Áo nỉ - Áo len nam / Đồ bơi - Đồ đi biển nam /
+                Quần áo nam trung niên / Quần áo nam kích cỡ lớn
+              </p>
+            </div>
+            <div>
+              <h5>Giày - Dép nữ</h5>
+              <p>
+                Giày cao gót / Dép - Guốc nữ / Giày thể thao nữ / Giày sandals
+                nữ / Giày búp bê / Giày boots nữ / Giày lười nữ / Phụ kiện giày
+                / Giày Đế xuồng nữ
+              </p>
+            </div>
+            <div>
+              <h5>Giày - Dép nam</h5>
+              <p>
+                Giày lười nam / Giày tây nam / Giày thể thao nam / Dép nam /
+                Giày sandals nam / Phụ kiện giày nam / Giày boots nam
+              </p>
+            </div>
+            <div>
+              <h5>Túi thời trang nữ</h5>
+              <p>
+                Túi đeo chéo, túi đeo vai nữ / Ví nữ / Túi xách tay nữ / Túi
+                tote nữ / Phụ kiện túi
+              </p>
+            </div>
+            <div>
+              <h5>Túi thời trang nam</h5>
+              <p>
+                Ví nam / Túi đeo chéo nam / Túi xách công sở nam / Túi bao tử,
+                túi đeo bụng
+              </p>
+            </div>
+          </Col>
+          <Col span={4.8}>
+            <div>
+              <h5>Balo và Vali</h5>
+              <p>
+                Balo / Túi du lịch và phụ kiện / Balo, cặp, túi chống sốc laptop
+                / Vali, phụ kiện vali
+              </p>
+            </div>
+            <div>
+              <h5>Phụ kiện thời trang</h5>
+              <p>Phụ kiện thời trang nữ / Mắt kính / Phụ kiện thời trang nam</p>
+            </div>
+            <div>
+              <h5>Đồng hồ và Trang sức</h5>
+              <p>
+                Trang sức / Đồng hồ nữ / Đồng hồ nam / Phụ kiện đồng hồ / Đồng
+                hồ trẻ em
+              </p>
+            </div>
+            <div>
+              <h5>Laptop - Máy Vi Tính - Linh kiện</h5>
+              <p>
+                Linh Kiện Máy Tính - Phụ Kiện Máy Tính / Thiết Bị Văn Phòng -
+                Thiết Bị Ngoại Vi / Thiết Bị Mạng / Thiết Bị Lưu Trữ / PC - Máy
+                Tính Bộ / Laptop
+              </p>
+            </div>
+            <div>
+              <h5>Nhà Cửa - Đời Sống</h5>
+              <p>
+                Trang trí nhà cửa / Dụng cụ nhà bếp / Sửa chữa nhà cửa / Ngoài
+                trời & sân vườn / Nội thất / Đèn & thiết bị chiếu sáng / Đồ dùng
+                và thiết bị nhà tắm / Đồ dùng phòng ăn / Đồ dùng phòng ngủ / Đồ
+                thờ cúng / Nhạc cụ / Hoa tươi và cây cảnh
+              </p>
+            </div>
+            <div>
+              <h5>Bách Hóa Online</h5>
+              <p>
+                Chăm sóc thú cưng / Đồ uống - Pha chế dạng bột / Thực phẩm Đóng
+                hộp và Khô / Gia Vị và Chế Biến / Đậu & Hạt Các Loại / Đồ Ăn Vặt
+                / Sữa và các Sản phẩm từ sữa / Đồ Uống Không Cồn / Đồ uống có
+                cồn / Bộ quà tặng
+              </p>
+            </div>
+          </Col>
+          <Col span={4.8}>
+            <div>
+              <h5>Hàng Quốc Tế</h5>
+              <p>
+                Nhà Cửa - Đời Sống / Thời Trang / Ô tô, xe máy, xe đạp / Thiết
+                Bị Số - Phụ Kiện Số / Thể thao - Dã ngoại / Làm Đẹp - Sức Khỏe /
+                Sách, Văn phòng phẩm & Quà lưu niệm / Mẹ & Bé / Điện gia dụng /
+                Bách hóa online / Máy Ảnh - Máy Quay Phim / Laptop & Máy Vi Tính
+                / Sản phẩm - thiết bị công nghiệp / Tivi & Thiết Bị Nghe Nhìn /
+                Điện Thoại - Máy Tính Bảng
+              </p>
+            </div>
+            <div>
+              <h5>Thiết Bị Số - Phụ Kiện Số</h5>
+              <p>
+                Phụ Kiện Điện Thoại và Máy Tính Bảng / Phụ kiện máy tính và
+                Laptop / Thiết Bị Âm Thanh và Phụ Kiện / Thiết Bị Thông Minh và
+                Linh Kiện Điện Tử / Thiết Bị Đeo Thông Minh và Phụ Kiện / Thiết
+                Bị Chơi Game và Phụ Kiện
+              </p>
+            </div>
+            <div>
+              <h5>Voucher - Dịch vụ</h5>
+              <p>
+                Thanh toán hóa đơn / Khóa học / Du lịch - Khách sạn / Spa & Làm
+                đẹp / Dịch vụ khác / Nhà hàng - Ăn uống / Sự kiện - Giải trí /
+                Nha khoa - Sức khỏe / Phiếu quà tặng / Sim số - Thẻ cào - Thẻ
+                game
+              </p>
+            </div>
+            <div>
+              <h5>Ô Tô - Xe Máy - Xe Đạp</h5>
+              <p>
+                Phụ kiện - Chăm sóc xe / Xe điện / Xe đạp / Xe máy / Ô tô / Xe
+                Scooter / Dịch vụ, lắp đặt
+              </p>
+            </div>
+            <div>
+              <h5>Nhà Sách Tiki</h5>
+              <p>
+                Sách tiếng Việt / Văn phòng phẩm / Quà lưu niệm / English Books
+              </p>
+            </div>
+          </Col>
+          <Col span={4.8}>
+            <div>
+              <h5>Điện Tử - Điện Lạnh</h5>
+              <p>
+                Âm thanh & Phụ kiện Tivi / Phụ kiện điện lạnh / Tủ lạnh / Máy
+                lạnh - Máy điều hòa / Máy giặt / Tủ đông - Tủ mát / Tivi / Máy
+                nước nóng / Máy rửa chén / Máy sấy quần áo / Tủ ướp rượu
+              </p>
+            </div>
+            <div>
+              <h5>Thể Thao - Dã Ngoại</h5>
+              <p>
+                Trang phục thể thao nữ / Trang phục thể thao nam / Đồ dùng dã
+                ngoại / Giày thể thao nam / Phụ kiện thể thao / Dụng cụ - thiết
+                bị tập thể thao / Các môn thể thao khác / Dụng cụ câu cá / Thể
+                thao dưới nước / Các môn thể thao đồng đội / Các môn thể thao
+                chơi vợt / Các môn thể thao đối kháng / Giày thể thao nữ / Thực
+                phẩm bổ sung năng lượng / Dụng cụ leo núi
+              </p>
+            </div>
+            <div>
+              <h5>Máy Ảnh - Máy Quay Phim</h5>
+              <p>
+                Phụ Kiện Máy Ảnh, Máy Quay / Camera Giám Sát / Thiết Bị Ánh Sáng
+                / Camera Hành Trình - Action Camera và Phụ Kiện / Balo - Túi
+                Đựng - Bao Da / Ống Kính - Ống Ngắm / Ống Kính (Lens) / Thiết Bị
+                Quay Phim / Máy Bay Camera và Phụ Kiện / Máy Ảnh
+              </p>
+            </div>
+            <div>
+              <h5>Từ Khóa Được Quan Tâm</h5>
+              <p>
+                máy sưởi / đèn sưởi / quạt tích điện sunhouse / máy sấy tóc
+                philips / bàn ủi hơi nước đứng / bàn là hơi nước philip / may
+                sưởi xiaomi / bình nươc nóng ariston / máy sưởi xiaomi / máy hút
+                bụi giường nệm / cây nước nóng lạnh toshiba / robot hút bụi lau
+                nhà của nhật / đèn sưởi âm trần / nồi cơm điện tefal
+              </p>
+            </div>
+          </Col>
         </Row>
       </div>
     </>
